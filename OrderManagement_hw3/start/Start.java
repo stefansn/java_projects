ad23066fe5346b4dcc4ed60865918aab6e5eacd8
stefansn <41380897+stefansn@users.noreply.github.com>
2018-08-17 10:58:09 +0300

    Update Start.java

--- OrderManagement_hw3/start/Start.java
+++ OrderManagement_hw3/start/Start.java
@@ -19,25 +19,10 @@ public class Start {
 	public static void main(String[] args) {	
 			ClientBll clb = new ClientBll();
 			
-			//clb.insertIntoClient(new Client(6,"Stefan","Stanescu"));
-			//clb.insertIntoClient(new Client(7,"Jake","Jake"));
-			//clb.deleteByIdClient(7);
+			
 			
 			ComandaBll comb = new ComandaBll();
-//			comb.insertIntoComanda(new Comanda(1,1,1,100));
-			
-			//clb.insertIntoClient(new Client("Coltan","Gheorghe"));
-			//clb.updateClient(new Client("Aron","Oana"), 0);
-			//clb.insertIntoClient(new Client("Marcel","99"));\
-			//clb.insertIntoClient(new Client("Cristian","Puscas"));
-			//clb.insertIntoClient(new Client("Viorica","Dancila"));
-			
-			//clb.insertIntoClient(new Client("Bianca","Ioana"));
-			//clb.insertIntoClient(new Client("Oancea","Raul"));
-			//clb.deleteByIdClient(0);
-			//clb.orderByIdClient();
-			
-		//	comb.insertIntoComanda(new Comanda(2,2,500));
+
 			
 			View.createAndShowGui();
 	}
